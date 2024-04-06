102c151a788d50178c035ced232c19f8b53012a6
OmniaCrystalline <125163569+OmniaCrystalline@users.noreply.github.com>
2024-04-07 00:10:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ const PORT = 3000;
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 const transporter = nodemailer.createTransport({
   host: "smtp.hostinger.com",
   port: 465,
